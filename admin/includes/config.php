cb9b1dc702cadbd1366d6ebe0a54bc4d3f171f23
jasonleeluohwoei <80889610+jasonleeluohwoei@users.noreply.github.com>
2022-09-12 21:48:05 +0800

    Config update for live code

--- admin/includes/config.php
+++ admin/includes/config.php
@@ -1,28 +1,28 @@
 <?php
 // DB credentials.
-define('DB_HOST', 'localhost');
-define('DB_USER', 'root');
-define('DB_PASS', '');
-define('DB_NAME', 'spa');
-// Establish database connection.
-try {
-    $dbh = new PDO("mysql:host=" . DB_HOST . ";dbname=" . DB_NAME, DB_USER, DB_PASS, array(PDO::MYSQL_ATTR_INIT_COMMAND => "SET NAMES 'utf8'"));
-} catch (PDOException $e) {
-    exit("Error: " . $e->getMessage());
-}
-
-?>
-
-<?php
-// DB credentials.
 // define('DB_HOST', 'localhost');
-// define('DB_USER', 'smartp85_smartp85');
-// define('DB_PASS', 'I163[zAp-9SjnG');
-// define('DB_NAME', 'smartp85_spa');
+// define('DB_USER', 'root');
+// define('DB_PASS', '');
+// define('DB_NAME', 'spa');
 // // Establish database connection.
 // try {
 //     $dbh = new PDO("mysql:host=" . DB_HOST . ";dbname=" . DB_NAME, DB_USER, DB_PASS, array(PDO::MYSQL_ATTR_INIT_COMMAND => "SET NAMES 'utf8'"));
 // } catch (PDOException $e) {
 //     exit("Error: " . $e->getMessage());
 // }
+
+?>
+
+<?php
+// DB credentials.
+define('DB_HOST', 'localhost');
+define('DB_USER', 'smartp85_smartp85');
+define('DB_PASS', 'I163[zAp-9SjnG');
+define('DB_NAME', 'smartp85_spa');
+// Establish database connection.
+try {
+    $dbh = new PDO("mysql:host=" . DB_HOST . ";dbname=" . DB_NAME, DB_USER, DB_PASS, array(PDO::MYSQL_ATTR_INIT_COMMAND => "SET NAMES 'utf8'"));
+} catch (PDOException $e) {
+    exit("Error: " . $e->getMessage());
+}
 ?> 
