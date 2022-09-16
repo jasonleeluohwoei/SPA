6a34aa70bae8f633fa9244315d90bbaa2b6b3f22
hongjk1029 <hongjunkhuan@gmail.com>
2022-09-16 22:46:25 +0800

    update on role management

---
+++ admin/da8f3a12b42ccf32cc4b533dbac987bb/includes/leftbar.php
@@ -0,0 +1,22 @@
+	<nav class="ts-sidebar">
+		<ul class="ts-sidebar-menu">
+
+			<li class="ts-label">Main</li>
+
+			<li><a href="#"><i class="fa fa-files-o"></i> Brands</a>
+				<ul>
+					<li><a href="create-brand.php">Create Brand</a></li>
+					<li><a href="manage-brands.php">Manage Brands</a></li>
+				</ul>
+			</li>
+
+			<li><a href="#"><i class="fa fa-car"></i> Vehicles</a>
+				<ul>
+					<li><a href="post-avehical.php">Post a Vehicle</a></li>
+					<li><a href="manage-vehicles.php">Manage Vehicles</a></li>
+				</ul>
+			</li>
+
+
+		</ul>
+	</nav>
--- admin/index.php
+++ admin/index.php
@@ -11,16 +11,18 @@ $query-> bindParam(':email', $email, PDO::PARAM_STR);
 $query-> bindParam(':password', $password, PDO::PARAM_STR);
 $query-> execute();
 $results=$query->fetchAll(PDO::FETCH_OBJ);
+
 if($query->rowCount() > 0)
 {
-$_SESSION['alogin']=$_POST['username'];
-echo "<script type='text/javascript'> document.location = 'dashboard.php'; </script>";
+	if($_SESSION['alogin']=$_POST['username']=='admin'){
+			echo "<script type='text/javascript'> document.location = 'dashboard.php'; </script>";
+	}else if($_SESSION['alogin'] = $_POST['username'] == 'superadmin'){
+			echo "<script type='text/javascript'> document.location = 'da8f3a12b42ccf32cc4b533dbac987bb/post-avehical.php'</script>";
+	}
 } else{
-  
   echo "<script>alert('Invalid Details');</script>";
 
 }
-
 }
 
 ?>
